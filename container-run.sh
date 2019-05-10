7b1241b518b51df7a0b67cd7e293df883480af1d
east4ming <cuikaidong@foxmail.com>
2019-05-10 08:14:50 +0800

    add port 443

--- container-run.sh
+++ container-run.sh
@@ -1,4 +1,4 @@
 # DELETE previous container
 sudo docker rm -f my-tencentyun-nginx
 # EXPOSE 80
-sudo docker run --name my-tencentyun-nginx -v /etc/pki/nginx:/etc/pki/nginx:ro -d -p 80:80 caseycui/my-tencentyun-nginx
+sudo docker run --name my-tencentyun-nginx -v /etc/pki/nginx:/etc/pki/nginx:ro -d -p 80:80 -p 443:443 caseycui/my-tencentyun-nginx
